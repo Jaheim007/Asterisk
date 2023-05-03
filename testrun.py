71211670be43225289799f77582b6021f18c358d
Jaheim007 <jaheimkouaho@gmail.com>
2023-05-03 17:12:45 +0000

    First Test of MYSQL with asterisk-ami

--- testrun.py
+++ testrun.py
@@ -1,29 +1,61 @@
 import mysql.connector
+from asterisk.ami import AMIClient 
 
-def mysqlconnector():
-    try:                                                                         
-        mydb = mysql.connector.connect(
-            host = "54.36.181.102", 
-            user = "Jaheim1",
-            password = "root",
-            database = "UserAsterisk_db"
-        )
+client = AMIClient(address="54.36.181.102", port=5038)
+client.login(username='manager' , secret='im')
+                                                  
+mydb = mysql.connector.connect(
+    host = "54.36.181.102", 
+    user = "Jaheim1",
+    password = "root",
+    database = "UserAsterisk_db"
+)
 
-        #Cursor Point Connection
-        mycursor = mydb.cursor()
+caller_id = "SIP/Admin"
+cursor = mydb.cursor()
+cursor.execute(f"SELECT CashAmount FROM Accounts WHERE caller_id='{caller_id}'")
+result = cursor.fetchone()
 
-        #Read Items in the Table 
-        mycursor.execute("SELECT * FROM Accounts")
-        myresults = mycursor.fetchall()
-
-        
-        for item in myresults:
-            print(item)
-
-    except Exception as e:
-        print(e)
+if result is not None:
+    call_duration = result[0]
+else:
+    print("You're Broke")
+    exit()
     
-print(mysqlconnector())
+action = {
+    'Action' : 'Originate', 
+    'Channel' : 'SIP/Admin',
+    'Context' : 'Trial',
+    'Exten' : '1234',
+    'Priority': '1',
+    'SetVar': f'CALL_TIMEOUT={int(call_duration*60)}'
+}
+
+response = client.send_action(action)
+if response.is_error():
+    print(response.get_error())
+else:
+    print(f"Dialing {action['Channel']} with {int(call_duration*60)} seconds timeout")
+
+# Disconnect from Asterisk Manager Interface
+client.logoff()
+client.close()
+
+# Close MySQL database connection
+mydb.close()
+
+
+#Cursor Point Connection
+# mycursor = mydb.cursor()
+
+# #Read Items in the Table 
+# mycursor.execute("SELECT * FROM Accounts")
+# myresults = mycursor.fetchall()
+
+
+# for item in myresults:
+#     print(item)
+    
     
     
     
