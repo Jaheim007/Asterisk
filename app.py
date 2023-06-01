2db81da3713d3f8bb53fb9f90c69b37b2323fc42
Jaheim007 <jaheimkouaho@gmail.com>
2023-06-01 11:45:45 +0000

    Adding the AbsoulteTimeout in the app.py on the main branch

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from pyami_asterisk import AMIClient
 
 def all_events(events):
-    print(events)
+    if events.get('Event') == "BridgeEnter":
+        ami.create_action({
+            "Action":"AbsoluteTimeout",
+            "Channel": events.get('Channel'),
+            "Timeout" : "5"
+        },callback_originate
+    )
 
 def callback_originate(events):
     print(events)
+    
+def get_event(events):
+    if events.get('Event') == "BridgeEnter":
+        ami.create_action({
+            "Action":"AbsoluteTimout",
+            "Channel" : 'Channel',
+            "Timeout" : "5"
+        },callback_originate
+    )
 
 ami = AMIClient(host='54.36.181.102', port=5038, username='1024' , secret='1024')
 
@@ -19,7 +34,7 @@ ami.create_action(
         "Priority": "1",
         "CallerID": "Python"
     },
-    callback_originate,
+    all_events,
 )
 
 ami.register_event(["*"], all_events)
--- test.py
+++
@@ -1,9 +0,0 @@
-# def solution():
-#     sequence = [1,3,2]
-#     sort_list = sorted(sequence)
-#     print(sort_list)
-    
-#     for i in sort_list:
-        
-   
-# solution()
