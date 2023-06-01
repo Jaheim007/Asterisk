f4a8d83e72e050fbd8cebca5510c34fa19d98280
Jaheim007 <jaheimkouaho@gmail.com>
2023-06-01 14:33:06 +0000

    Trying to copy from my other branch

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 from pyami_asterisk import AMIClient
 
-def all_events(events):
-    if events.get('Event') == "BridgeEnter":
-        ami.create_action({
-            "Action":"AbsoluteTimeout",
-            "Channel": events.get('Channel'),
-            "Timeout" : "5"
-        },callback_originate
-    )
-
 def callback_originate(events):
     print(events)
     
 def get_event(events):
-    if events.get('Event') == "BridgeEnter":
-        ami.create_action({
-            "Action":"AbsoluteTimout",
-            "Channel" : 'Channel',
-            "Timeout" : "5"
-        },callback_originate
-    )
+    print(events.get('Event'))
+    
+def all_events(events):
+    print(events)
 
 ami = AMIClient(host='54.36.181.102', port=5038, username='1024' , secret='1024')
 
@@ -28,19 +16,13 @@ ami.create_action(
         "Action": "Originate",
         "Channel": "SIP/101",
         "Timeout": "20000",
-        "Exten": "4000",
+        "Exten": "7000",
         "Context": "Tester",
         "Async": "true",
         "Priority": "1",
-        "CallerID": "Python"
     },
-    all_events,
+    callback_originate,
 )
-
-ami.register_event(["*"], all_events)
-
+# ami.register_event(["*"], all_events)
 ami.connect()
-
-
-
 
